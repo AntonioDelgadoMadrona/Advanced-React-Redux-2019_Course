e999258226c6e93a91f19f84440d94cfb5f36088
tonidelgadomadrona <50912526+tonidelgadomadrona@users.noreply.github.com>
2020-01-14 09:59:17 +0100

    InProgress

--- section01-testing/src/components/CommentBox.js
+++ section01-testing/src/components/CommentBox.js
@@ -1,7 +1,36 @@
 import React from 'react';
 
-export default () => {
+const CommentBox = () => {
+
+    const initialState = {
+        comment: '',
+    };
+
+    const [state, setState] = React.useState(initialState);
+
+    const handleChange = e => {
+        const { value, name } = e.target;
+        setState(prevState => ({
+            ...prevState,
+            [name]: value
+        }))
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        setState(initialState);
+    };
+
     return (
-        <div>Comment Box</div>
+        <form onSubmit={handleSubmit}>
+            <h4>Add a Comment</h4>
+            <textarea name="comment" value={state.comment} onChange={handleChange} />
+            <div>
+                <button>Submit Comment</button>
+            </div>
+        </form>
     )
 };
+
+export default CommentBox;
--- section01-testing/src/components/__tests__/App.test.js
+++ section01-testing/src/components/__tests__/App.test.js
@@ -1,17 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../App';
+import { shallow } from 'enzyme';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
+
+// FOR TEST THAT A COMPONENT RENDER A TEXT (NOT RECOMMENDED)
+// it('shows a comment box', () => {
+//     const div = document.createElement('div');
+
+//     ReactDOM.render(<App />, div);
+
+//     // Looks inside the div
+//     // and checks to see if the CommentBox is in there
+
+//     // console.log(div.innerHTML);
+//     expect(div.innerHTML).toContain('Comment Box');
+
+//     ReactDOM.unmountComponentAtNode(div);
+// })
+
+let wrapped;
+
+beforeEach(() => {
+    wrapped = shallow(<App />);
+});
 
 it('shows a comment box', () => {
-    const div = document.createElement('div');
+    expect(wrapped.find(CommentBox).length).toEqual(1);
+});
 
-    ReactDOM.render(<App />, div);
-
-    // Looks inside the div
-    // and checks to see if the CommentBox is in there
-
-    // console.log(div.innerHTML);
-    expect(div.innerHTML).toContain('Comment Box');
-
-    ReactDOM.unmountComponentAtNode(div);
-})
+it('shows a comment list', () => {
+    expect(wrapped.find(CommentList).length).toEqual(1);
+});
